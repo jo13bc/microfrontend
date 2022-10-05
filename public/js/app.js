b3075333f10085f5ccdf6508190ab321a8c979c7
José Beita <jo13bc@gmail.com>
2022-10-04 20:21:50 -0600

    Ajuste

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,3 @@
-function checkId(item) {
-  return item.id == this;
-}
-
-function getById(query) {
-  let params = query.params;
-  if (params) {
-    fetch("/template/detail.html")
-      .then((response) => response.text())
-      .then((template) => {
-        let elem = data.find(checkId, params.id);
-        let rendered = Mustache.render(template, elem);
-        document.getElementById("content").innerHTML = rendered;
-      });
-  } else {
-    fetch("/template/list.html")
-      .then((response) => response.text())
-      .then((template) => {
-        let rendered = Mustache.render(template, data);
-        document.getElementById("content").innerHTML = rendered;
-      });
-  }
-}
-
-function init() {
-  router = new Navigo("/");
-  router.on("/", getById).resolve();
+function redirect(page) {
+  document.getElementById("iframe").src = `https://${page}-microsite.netlify.app`;
 }
