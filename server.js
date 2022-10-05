b3114a53fa3d82b98d38dd32c6571f1839bd219c
José Beita <jo13bc@gmail.com>
2022-10-04 21:15:42 -0600

    Ajuste

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use("/css", express.static(__dirname + "/public/css"));
 app.use("/js", express.static(__dirname + "/public/js"));
-app.use("/images", express.static(__dirname + "/public/images"));
 app.use(express.static('public'));
 
 let server = app.listen(port, () => console.log("Server started at http://localhost:%s", port));
