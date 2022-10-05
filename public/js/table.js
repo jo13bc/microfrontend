b6f5fcdf6d5b49f68a7b386d2fa7fc6813401f3b
José Beita <jo13bc@gmail.com>
2022-10-04 19:27:03 -0600

    Desarrollo

--- public/js/table.js
+++ public/js/table.js
@@ -1,120 +1,95 @@
 const data = [
   {
     id: 1,
-    name: "Bichos - Una aventura en miniatura",
-    director: { id: 4, name: "John Lasseter" },
-    studies: [
-      { id: 2, name: "Walt Disney Pictures" },
-      { id: 4, name: "Pixar Animation Studios" },
+    name: "Brad Bird",
+    birth_year: "24 de septiembre de 1957",
+    nationality: "Estadounidense",
+    movies: [
+      { id: 9, name: "Ratatouille" },
+      { id: 10, name: "Los increíbles" },
     ],
-    image: "bichos.jpeg",
+    image: "BradBird.jpg",
   },
   {
     id: 2,
-    name: "Lluvia de hamburguesas",
-    director: { id: 6, name: "Phil Lord" },
-    studies: [
-      { id: 5, name: "Sony Pictures Home Entertainment" },
+    name: "Erico Casarosa",
+    birth_year: "19 de octubre de 1971",
+    nationality: "Italiano",
+    movies: [
+      { id: 4, name: "Luca" },
     ],
-    image: "CloudyWithAchanceOfMeatballs.jpg",
+    image: "EnricoCasarosa.jpg",
   },
   {
     id: 3,
-    name: "Kung Fu Panda 2",
-    director: { id: 3, name: "Jennifer Yuh Nelson" },
-    studies: [
-      { id: 1, name: "21st Century Fox" },
-      { id: 3, name: "DreamWorks" },
+    name: "Jennifer Yuh Nelson",
+    birth_year: "1972",
+    nationality: "Sur Coreana",
+    movies: [
+      { id: 3, name: "Kung Fu Panda 2" },
     ],
-    image: "Kung_fu_panda.jpeg",
+    image: "Jennifer_Yuh_Nelson.jpg",
   },
   {
     id: 4,
-    name: "Luca",
-    director: { id: 2, name: "Erico Casarosa" },
-    studies: [
-      { id: 2, name: "Walt Disney Pictures" },
-      { id: 4, name: "Pixar Animation Studios" },
+    name: "John Lasseter",
+    birth_year: "1957",
+    nationality: "Estadounidense",
+    movies: [
+      { id: 1, name: "Bichos - Una aventura en miniatura" },
+      { id: 11, name: "Toy Story" },
     ],
-    image: "Luca.jpg",
+    image: "JohnLasseter.jpg",
   },
   {
     id: 5,
-    name: "Los Minios",
-    director: { id: 7, name: "Pierre Coffin" },
-    studies: [
-      { id: 6, name: "Universal Pictures" },
+    name: "Pete Docter",
+    birth_year: "9 de octubre de 1968",
+    nationality: "Estadounidense",
+    movies: [
+      { id: 7, name: "Monsters Inc" },
+      { id: 12, name: "UP - una aventura de altura" },
     ],
-    image: "minions.jpeg",
+    image: "petedocter.png",
   },
   {
     id: 6,
-    name: "Moana",
-    director: { id: 9, name: "Ron Clements" },
-    studies: [
-      { id: 2, name: "Walt Disney Pictures" },
-      { id: 7, name: "Walt Disney Animation Studios" },
+    name: "Phil Lord",
+    birth_year: "12 de julio de 1975",
+    nationality: "Estadounidense",
+    movies: [
+      { id: 2, name: "Lluvia de hamburguesas" },
     ],
-    image: "Moana.jpg",
+    image: "PhilLord.png",
   },
   {
     id: 7,
-    name: "Monsters Inc",
-    director: { id: 5, name: "Pete Docter" },
-    studies: [
-      { id: 2, name: "Walt Disney Pictures" },
-      { id: 4, name: "Pixar Animation Studios" },
+    name: "Pierre Coffin",
+    birth_year: "1 de noviembre de 1967",
+    nationality: "Francés",
+    movies: [
+      { id: 5, name: "Los Minios" },
     ],
-    image: "monstersinc.webp",
+    image: "pierrecoffin.jpeg",
   },
   {
     id: 8,
-    name: "Señor Peabody y Sherman",
-    director: { id: 8, name: "Rob Minkoff" },
-    studies: [
-      { id: 1, name: "21st Century Fox" },
-      { id: 3, name: "DreamWorks" },
+    name: "Rob Minkoff",
+    birth_year: "11 de agosto de 1962",
+    nationality: "Estadounidense",
+    movies: [
+      { id: 8, name: "Señor Peabody y Sherman" },
     ],
-    image: "PeabodyAndSherman.jpg",
+    image: "RobMinkoff.jpg",
   },
   {
     id: 9,
-    name: "Ratatouille",
-    director: { id: 1, name: "Brad Bird" },
-    studies: [
-      { id: 2, name: "Walt Disney Pictures" },
-      { id: 4, name: "Pixar Animation Studios" },
+    name: "Ron Clements",
+    birth_year: "25 de abril de 1953",
+    nationality: "Estadounidense",
+    movies: [
+      { id: 6, name: "	Moana" },
     ],
-    image: "Ratatouille.jpeg",
-  },
-  {
-    id: 10,
-    name: "Los increíbles",
-    director: { id: 1, name: "Brad Bird" },
-    studies: [
-      { id: 2, name: "Walt Disney Pictures" },
-      { id: 4, name: "Pixar Animation Studios" },
-    ],
-    image: "TheIncredibles.jpg",
-  },
-  {
-    id: 11,
-    name: "Toy Story",
-    director: { id: 4, name: "John Lasseter" },
-    studies: [
-      { id: 2, name: "Walt Disney Pictures" },
-      { id: 4, name: "Pixar Animation Studios" },
-    ],
-    image: "ToyStory.jpg",
-  },
-  {
-    id: 12,
-    name: "UP - una aventura de altura",
-    director: { id: 5, name: "Pete Docter" },
-    studies: [
-      { id: 2, name: "Walt Disney Pictures" },
-      { id: 4, name: "Pixar Animation Studios" },
-    ],
-    image: "Up.jpeg",
+    image: "RonClements.jpg",
   },
 ];
